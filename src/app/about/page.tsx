348907447bb0fb536f4e7b8d758c82260da58f9f
retroazure <tomascosta005@outlook.com>
2024-10-02 16:53:18 +0100

    added more About me Content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import Image from 'next/image';
 
+const images = [
+  '/assets/surfing_wave.jpg',
+  '/assets/dolphin.jpg',
+  '/assets/chosen_1.jpg',
+  '/assets/chosen_4.jpg',
+  '/assets/chosen_3.jpg',
+  '/assets/scenery_3.jpg',
+  '/assets/scenery_5.jpg',
+  '/assets/scenery_2.jpg',
+];
 
 const About: React.FC = () => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
   return (
     <>
       <section className="relative w-full h-[750px] pt-16 md:pt-0">
         <Image
-          src="/assets/background.webp" // Replace with your actual image path
+          src="/assets/background.jpg"
           alt="Background Image"
           fill
           style={{ objectFit: 'cover', opacity: 0.85 }}
@@ -29,6 +42,37 @@ const About: React.FC = () => {
           We offer quality tours, provided by our partners, tailored to the profile of our guests.
         </p>
       </section>
+
+      <section className="w-full h-auto p-8 md:p-16 bg-lightGray">
+        <h2 className="font-bellota text-3xl md:text-5xl text-center mb-8">Gallery</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-0">
+          {images.map((src, index) => (
+            <div key={index} className="relative w-full h-[750px] md:h-[550px] lg:h-[650px] mx-auto md:w-[95%] lg:w-[85%] mb-4" onClick={() => setSelectedImage(src)}>
+              <Image
+                src={src}
+                alt={`Image ${index + 1}`}
+                fill
+                style={{ objectFit: 'cover' }}
+                className="rounded-lg cursor-pointer"
+              />
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {selectedImage && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50" onClick={() => setSelectedImage(null)}>
+          <div className="relative w-3/4 h-3/4">
+            <Image
+              src={selectedImage}
+              alt="Selected Image"
+              fill
+              style={{ objectFit: 'contain' }}
+              className="rounded-lg"
+            />
+          </div>
+        </div>
+      )}
     </>
   );
 };
